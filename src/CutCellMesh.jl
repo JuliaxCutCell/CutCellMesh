38cba1f51e4c77fb2a3b70e6cb0ec0fc1415b1b3
Fastaxx <Fastaxx@github.com>
2024-04-05 11:49:58 +0200

    clean

--- src/CutCellMesh.jl
+++ src/CutCellMesh.jl
@@ -14,39 +14,4 @@ export CartesianGrid, generate_mesh, cell_boundary_indices, cell_volumes, get_ed
 export plot_mesh
 export interpolate_values
 
-
-# Cas 1D
-grid = CartesianGrid((10,), (0.1,))
-mesh = generate_mesh(grid)
-@show mesh
-
-plot_mesh(mesh)
-readline()
-
-edge = get_edges(mesh)
-@show edge
-
-# Cas 2D
-grid = CartesianGrid((10, 10), (0.1, 0.1))
-mesh = generate_mesh(grid)
-@show mesh
-
-edge = get_edges(mesh)
-@show edge
-
-"""
-# Cas 3D
-grid = CartesianGrid((10, 10, 10), (0.1, 0.1, 0.1))
-mesh = generate_mesh(grid)
-@show mesh
-
-node = nodes(grid)
-@show node
-
-"""
-
-
-
-#plot_mesh(mesh)
-#readline()
 end # module CutCellMesh
